005a66031e0d20ff18e0c20f34b98407d7cae97d
imxamanda <amandavieiraperes.12@gmail.com>
2024-03-15 21:44:53 -0300

    mongoose

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+const conn = require('./db/conn')
+conn()
+
+app.get('/', function(req, res) {
+    res.json({msg: 'Rota Principal'})
+
+})
+
+app.listen(3000, function(){
+    console.log('Server is running on port')
+})
