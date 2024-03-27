f94d4ea3f015375fdaaf01434230a21be9ee9b04
imxamanda <amandavieiraperes.12@gmail.com>
2024-03-27 19:25:46 -0300

    .env att

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function main(){
 
     try {
-        await mongoose.connect(`mongodb+srv://amandaperes:TBrZjaqfVrhbiMIv@cluster0.t3uxlhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+        await mongoose.connect(`mongodb+srv://${process.env.login}:${process.env.senha}@cluster0.t3uxlhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
     console.log('Banco Ok');
         
     } catch (error) {
